c3cecdd0e0fa669271905b3ab89c017c2e7e8248
sha-r0 <rohitndls9873@gmail.com>
2026-07-03 22:07:57 +0530

    Setup Security done

--- src/lib/firebase-admin.js
+++ src/lib/firebase-admin.js
@@ -2,18 +2,29 @@ import { getApps, initializeApp, cert } from "firebase-admin/app";
 import { getFirestore } from "firebase-admin/firestore";
 import { getAuth } from "firebase-admin/auth";
 
-const firebaseAdminConfig = {
-  credential: cert({
-    projectId: process.env.FIREBASE_PROJECT_ID,
-    clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
-    privateKey: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, "\n"),
-  }),
-};
+if (
+  !process.env.FIREBASE_PROJECT_ID ||
+  !process.env.FIREBASE_CLIENT_EMAIL ||
+  !process.env.FIREBASE_PRIVATE_KEY
+) {
+  throw new Error(
+    "Firebase Admin environment variables are missing."
+  );
+}
 
 const app =
   getApps().length > 0
     ? getApps()[0]
-    : initializeApp(firebaseAdminConfig);
+    : initializeApp({
+        credential: cert({
+          projectId: process.env.FIREBASE_PROJECT_ID,
+          clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+          privateKey: process.env.FIREBASE_PRIVATE_KEY.replace(
+            /\\n/g,
+            "\n"
+          ),
+        }),
+      });
 
 export const adminDb = getFirestore(app);
 export const adminAuth = getAuth(app);
